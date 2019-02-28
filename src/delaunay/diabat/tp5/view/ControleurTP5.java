60ca679af785e0e9a1b2790fb85b7e7f81c70778
delasalle-sio-delaunay-p <delasalle.sio.delaunay.pierre@gmail.com>
2019-03-01 00:08:26 +0100

    Continuation TP5

--- src/delaunay/diabat/tp5/view/ControleurTP5.java
+++ src/delaunay/diabat/tp5/view/ControleurTP5.java
@@ -5,11 +5,14 @@ import delaunay.diabat.tp5.*;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class ControleurTP5 {
 
-    private MotsCroisesTP5 motsCroises;
+    private MotsCroisesTP5 mc;
 
     @FXML
     private GridPane monGridPane;
@@ -17,22 +20,49 @@ public class ControleurTP5 {
     @FXML
 	private void initialize() {
 
-    	motsCroises = MotsCroisesFactory.creerMotsCroises2x3();
+    	mc = MotsCroisesFactory.creerMotsCroises2x3();
 
     	for (Node n : monGridPane.getChildren())
     	{
     	   if (n instanceof TextField)
     	   {
 		    	TextField tf = (TextField) n ;
-		    	//int lig = ((int) n.getProperties().get("gridpane­row")) + 1;
-		    	//int col = ((int) n.getProperties().get("gridpane­column")) + 1;
-		    	int lig = monGridPane.getColumnIndex(n) + 1;
-		    	int col = monGridPane.getRowIndex(n) + 1;
+		    	int lig = ((int) n.getProperties().get("gridpane-row")) + 1;
+		    	int col = ((int) n.getProperties().get("gridpane-column")) + 1;
 
-
+		    	
+		    	// Bindings
+		    	
+		    	tf.textProperty().bindBidirectional(mc.propositionProperty(lig, col));
+		    	/*
+		    	// Tooltips
+                String horizDefinition = this.mc.getDefinition(lig, col, true);
+                String vertiDefinition = this.mc.getDefinition(lig, col, false);
+                if (!horizDefinition.equals("") && !vertiDefinition.equals("")) {
+                    horizDefinition += " / ";
+                }
+                tf.setTooltip(new Tooltip(horizDefinition + "" + vertiDefinition));
+                
+                */
+                tf.setOnMouseClicked((e) -> {
+                    this.clicCase(e);
+                });
     	   }
-    	}
-
-
+    	}	
 	}
+  
+	@FXML
+	public void clicCase(MouseEvent e) {
+		
+    	if (e.getButton() == MouseButton.MIDDLE)
+    	{
+	    	// C'est un clic "central"
+	    	TextField cs = (TextField) e.getSource();
+	    	int lig = GridPane.getRowIndex(cs);
+            int col = GridPane.getColumnIndex(cs);
+            this.mc.reveler(lig, col);
+            
+    	}
+	}
+	
 }
